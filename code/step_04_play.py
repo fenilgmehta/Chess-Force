d2b5ee379d88f09c7a48b1ad7ccbb82fe82ed1ff
Fenil Mehta <fenilgmehta@gmail.com>
2019-10-05 10:10:52 +0530

    Add step_04_play.py to play chess with the Neural Network model(s)

---
+++ code/step_04_play.py
@@ -0,0 +1,15 @@
+from typing import Union
+from pathlib import Path
+import sys
+import numpy as np
+
+import step_03a_ffnn
+
+
+# TODO
+def play_chess():
+    return
+
+
+if __name__ == "__main__":
+    play_chess()
