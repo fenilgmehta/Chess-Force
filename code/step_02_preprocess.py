ce9a549f2334231fc261f61e4222236eb1b3d01c
Fenil Mehta <fenilgmehta@gmail.com>
2019-10-05 09:52:58 +0530

    Add step_02_preprocess.py
    - This program will generate centi-pawn(CP) value for the boards using available engines. This CP value is used for training the Neural Network(NN).
    - This program will also be used to preprocess the board state for input to the NN.

---
+++ code/step_02_preprocess.py
@@ -0,0 +1,34 @@
+from typing import Union
+from pathlib import Path
+import sys
+import chess.pgn
+
+
+# TODO
+def load_pgn(pgn_file_path: Union[str, Path]):
+    return
+
+
+# TODO
+def iterate_pgn(pgn_obj: chess.pgn):
+    return
+
+
+# TODO
+def iterate_game(nth_game):
+    return
+
+
+# TODO
+def generate_boards(board: chess.Board):
+    return
+
+
+# TODO
+def encode_board_777(board) -> str:
+    return ""
+
+
+# TODO
+def preprocess_pgn(pgn_file_path: Union[str, Path], output_path: Union[str, Path]):
+    return
