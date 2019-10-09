8766138d3869f0c95be2b19e6e1d21f50aad5fad
Fenil Mehta <fenilgmehta@gmail.com>
2019-10-09 18:17:19 +0530

    Fix bug in evaluate(...) & Rename, Add and Implement new functions
    1. FIX bug: evaluate(...)
            - Used to return #±value instead of a CP score. Hence, now user can pass two parameters when creating the CustomEngine object which are used to convert the check mate score to CP value.
            - It used to return CP score from the perspective of the player to play, not from Whites perspective. This has been fixed so that CP value is returned from Whites perspective always.
    2. RENAME:
            - evaluate(...) ---> evaluate_board(...)
            - evaluate_normalized(...) ---> evaluate_normalized_board(...)
    3. Add new functions
            - evaluate_fen(...)
            - evaluate_normalized_fen(...)
    4. Implement
            - evaluate_board(...)
            - evaluate_fen(...)
            - evaluate_normalized_board(...)
            - evaluate_normalized_fen(...)

--- code/step_01_engine.py
+++ code/step_01_engine.py
@@ -1,14 +1,17 @@
-from typing import Union
-from pathlib import Path
-import sys
-import os
 import multiprocessing
-import pandas as pd
+import os
+import sys
+from pathlib import Path
+from typing import Union
+
 import chess.engine
+import numpy as np
 
 
 class CustomEngine:
     def __init__(self, src_path: Union[str, Path] = None,
+                 mate_score_max: int = 8500,
+                 mate_score_difference: int = 50,
                  cpu_cores: int = multiprocessing.cpu_count(),
                  hash_size_mb: int = 4096,
                  depth: int = 15,
@@ -18,6 +21,10 @@ class CustomEngine:
         self.MAX_DEPTH = depth
         self.MAX_ANALYSE_TIME = analyse_time  # time is in seconds
 
+        # MAX CP score witnessed = +7311
+        self.MATE_SCORE_MAX = mate_score_max  # max score any board can get after check mate
+        self.MATE_SCORE_DIFFERENCE = mate_score_difference  # minimum difference between CP value for boards with check mate possible in "n" and "n-1" moves
+
         if self.MAX_THREADS_TO_USE < 1:
             self.MAX_THREADS_TO_USE = 1
         if self.MAX_HASH_TABLE_SIZE < 16:
@@ -26,6 +33,10 @@ class CustomEngine:
             self.MAX_DEPTH = 1
         if self.MAX_ANALYSE_TIME <= 0:
             self.MAX_ANALYSE_TIME = 0.1
+        if (self.MATE_SCORE_MAX is None) or (self.MATE_SCORE_MAX <= 0):
+            self.MATE_SCORE_MAX = 8500
+        if (self.MATE_SCORE_DIFFERENCE is None) or (self.MATE_SCORE_DIFFERENCE <= 0):
+            self.MATE_SCORE_DIFFERENCE = 50
 
         if src_path is None:
             DEFAULT_ENGINE_PATH = Path(r"../chess_engines/Linux/stockfish_10_x64")
@@ -73,15 +84,36 @@ class CustomEngine:
         self.engine_obj.configure({"Hash": hash_size})
         return
 
-    def evaluate(self, board: chess.Board):
+    def evaluate_board(self, board: chess.Board):
         # https://python-chess.readthedocs.io/en/v0.25.0/engine.html#analysing-and-evaluating-a-position
-        info = self.engine_obj.analyse(board, chess.engine.Limit(time=self.MAX_ANALYSE_TIME, depth=self.MAX_DEPTH))
-        # print(info["score"])
-        return info["score"]
+        score1 = self.engine_obj.analyse(
+            board=board,
+            limit=chess.engine.Limit(time=self.MAX_ANALYSE_TIME, depth=self.MAX_DEPTH),
+            info=chess.engine.INFO_SCORE
+        ).score
 
-    # TODO
-    def evaluate_normalized(self, board: chess.Board):
-        return self.evaluate(board)
+        if score1.white().is_mate():
+            # white_sign = np.sign(info['score'].white().mate())
+            # The above statement returns:
+            # (1) +1 value if white mates black -> white win if optimal play
+            # (2) -1 value if black mates white -> black win if optimal play
+            return (self.MATE_SCORE_MAX * np.sign(score1.white().mate())) \
+                   + (score1.white().score(mate_score=0) * self.MATE_SCORE_DIFFERENCE)
+            # info['score'].white().score(mate_score=0)
+            # The above statement return:
+            # (1) +ve integer value if black mates white
+            # (2) -ve integer value if white mates black
+
+        return score1.white()
+
+    def evaluate_fen(self, board_fen: str):
+        return self.evaluate_board(chess.Board(board_fen))
+
+    def evaluate_normalized_board(self, board: chess.Board):
+        return self.evaluate_board(board) / self.MATE_SCORE_MAX
+
+    def evaluate_normalized_fen(self, board_fen: str):
+        return self.evaluate_board(chess.Board(board_fen)) / self.MATE_SCORE_MAX
 
     def finish(self):
         self.engine_obj.close()
@@ -96,16 +128,16 @@ if __name__ == "__main__":
     import time
 
     start = time.time()
-    print(engine_sf.evaluate(board))
-    print(engine_sf.evaluate(board))
-    print(engine_sf.evaluate(board))
-    print(engine_sf.evaluate(board))
-    print(engine_sf.evaluate(board))
-    print(engine_sf.evaluate(board))
-    print(engine_sf.evaluate(board))
-    print(engine_sf.evaluate(board))
-    print(engine_sf.evaluate(board))
-    print(engine_sf.evaluate(board))
+    print(engine_sf.evaluate_board(board))
+    print(engine_sf.evaluate_board(board))
+    print(engine_sf.evaluate_board(board))
+    print(engine_sf.evaluate_board(board))
+    print(engine_sf.evaluate_board(board))
+    print(engine_sf.evaluate_board(board))
+    print(engine_sf.evaluate_board(board))
+    print(engine_sf.evaluate_board(board))
+    print(engine_sf.evaluate_board(board))
+    print(engine_sf.evaluate_board(board))
     end = time.time()
     print(end - start)
 
