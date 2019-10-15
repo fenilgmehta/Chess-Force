16c8a04d448a99b077b4f4f34733901c3a21caff
Fenil Mehta <fenilgmehta@gmail.com>
2019-10-15 19:26:37 +0530

    Add Implementation of a few methods of FFNN class - refer description
    Methods implemented:
    - __init__()
    - generate_model()
    - train_model(...)
    - predict(...)
    - predict_board(...)
    - predict_fen(...)

--- code/step_03a_ffnn.py
+++ code/step_03a_ffnn.py
@@ -1,20 +1,30 @@
-from typing import Union
 from pathlib import Path
-import sys
+from typing import Union
+
 import chess
 import numpy as np
+from tensorflow import keras
 
 import step_02_preprocess as step_02
 
 
 # Feed Forward Neural Network
 class FFNN:
-    # TODO
     def __init__(self):
+        self.model: keras.Sequential = None
         return
 
-    # TODO
     def generate_model(self):
+        # define the keras model
+        self.model = keras.Sequential()
+        self.model.add(keras.Dense(256, input_dim=777, activation='relu'))
+        self.model.add(keras.Dense(256, activation='relu'))
+        self.model.add(keras.Dense(256, activation='relu'))
+        self.model.add(keras.Dense(256, activation='relu'))
+        self.model.add(keras.Dense(1, activation='sigmoid'))
+
+        # compile the keras model
+        self.model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
         return
 
     # TODO
@@ -25,14 +35,15 @@ class FFNN:
     def load_model(self, model_path: Union[str, Path]):
         return
 
-    # TODO
-    def train_model(self, x_input, y_output):
+    def train_model(self, x_input, y_output, epochs, batch_size):
+        self.model.fit(x_input, y_output, epochs=epochs, batch_size=batch_size)
         return
 
-    # TODO
     def predict(self, encoded_board: str) -> np.float32:
-        return np.float32(0)
+        return self.model.predict(encoded_board)
 
-    # TODO
     def predict_board(self, board: chess.Board) -> np.float32:
         return self.predict(step_02.encode_board_777(board))
+
+    def predict_fen(self, board_fen: str) -> np.float32:
+        return self.predict(step_02.encode_board_777(chess.Board(board_fen)))
