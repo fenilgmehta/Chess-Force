74e1a2a0b88a5cd394a8277a26bfd938bc5ceb23
Fenil Mehta <fenilgmehta@gmail.com>
2019-11-13 22:57:59 +0530

    Add functions for cluster processing, Fix append_secondlast_line(...)
    - Move COLUMNS list from step_02_preprocess.py to common_services.cs as this list will be used everywhere

--- code/common_services.py
+++ code/common_services.py
@@ -1,12 +1,19 @@
 import os
-import sys
+import socket
 import subprocess
+import sys
 from pathlib import Path
 from typing import List, Union
 
+COLUMNS = ["fen_board", "cp_score"]
+
+
+###########################################################################################################
 
 def append_secondlast_line(resume_file_name, msg):
     # (last game written + 1), (next file number to be written)
+    if not Path(resume_file_name).exists():
+        savepoint(resume_file_name, msg)
     os.system(f"""
         last_line=`tail -n1 '{resume_file_name}'`
         sed --in-place '$d' '{resume_file_name}'
@@ -39,3 +46,29 @@ def readpoint(resume_file_name, variable_length) -> Union[List[int], int]:
               f"\n\tReturning only first '{len(result)}' values of {result}")
 
     return result[:variable_length]
+
+
+###########################################################################################################
+
+def print_ip_port_auth(file_to_write: Union[str, Path] = 'step_02_preprocess_server_ip.txt', your_port: int = None, your_authkey: str = None) -> None:
+    if your_port is None or not (isinstance(your_port, int)) or not (1000 <= your_port <= 65535):
+        raise ValueError("ERROR: your_port should be int -> [1000-65535]")
+    if your_authkey is None or not (isinstance(your_authkey, str)):
+        raise ValueError("ERROR: your_port should be any str object")
+
+    # Python Program to Get IP Address
+    hostname = socket.gethostname()
+    # IPAddr = socket.gethostbyname(hostname)
+    IPAddr = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2]
+                           if not ip.startswith("127.")][:1],
+                          [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
+    print(f"Your Computer Name is = '{hostname}'")
+    print(f"Your Computer IP Address is = '{IPAddr}'")
+    os.system(f"echo '{IPAddr},{your_port},{your_authkey}' > '{str(file_to_write)}'")
+
+
+def read_ip_port_auth(file_to_read) -> List[str]:
+    file1 = open(file_to_read, "r")
+    return file1.readline().strip('\n').strip().split(",")
+
+###########################################################################################################
