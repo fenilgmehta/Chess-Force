20e5921a7c26c6d91e023104e589b225d2d909f2
Fenil Mehta <fenilgmehta@gmail.com>
2019-12-11 19:35:29 +0530

    Add class KerasModels, Update FFNNKeras constructor, Fix predict methods
    - ADD class KerasModels
        - Class with only static methods which creates a Keras model and returns it to the called.
        - Allows one to use the same FFNNKeras class for different model structures.
        - Makes the program structure more modular.
    
    - UPDATE FFNNKeras constructor and FIX predict methods
        - Fix callback function filepath
        - Update callback function to store only the best model
        - Update training method to run the training on GPU
        - Add comments to `c_predict`
        - Update `c_predict_board_1`, `c_predict_board_n`, `c_predict_fen_1` and `c_predict_fen_n` to use the new `self.board_encoder` data member
    
    - Suppress numpy FutureWarning messages and TensorFlow warnings related to CPU instructions AVX2 FMA

--- code/step_03a_ffnn.py
+++ code/step_03a_ffnn.py
@@ -4,19 +4,36 @@ import warnings
 from pathlib import Path
 from typing import Union, Tuple, List
 
+# WARNING/ERROR: numpy FutureWarning
+# SOLUTION: https://github.com/tensorflow/tensorflow/issues/30427
+import tensorflow
+
+warnings.filterwarnings('ignore', category=FutureWarning)
+
+# WARNING/ERROR: Your CPU supports instructions that this TensorFlow binary was not compiled to use: AVX2 FMA
+# EXPLANATION: https://stackoverflow.com/questions/47068709/your-cpu-supports-instructions-that-this-tensorflow-binary-was-not-compiled-to-u
+# SOLUTION 1: https://stackoverflow.com/questions/51681727/tensorflow-on-macos-your-cpu-supports-instructions-that-this-tensorflow-binary?rq=1
+# SOLUTION 2: manually compile and install tensorflow 2.0
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# TENSORFLOW 2.0 installation with GPU support
+# Not tested SOLUTION: https://software.intel.com/en-us/articles/intel-optimization-for-tensorflow-installation-guide#Anaconda_main_win
+# conda install tensorflow -c anaconda
+# TESTED SOLUTION: https://anaconda.org/anaconda/tensorflow-gpu
+# conda install -c anaconda tensorflow-gpu
+
 import chess
 import numpy as np
 import pandas as pd
 import torch
 import torch.nn
-# import tensorflow
+import tensorflow as tf
 from tensorflow.python import keras
 from tensorflow.python.client import device_lib
 import argparse
 
 import common_services as cs
 import step_02_preprocess as step_02
-
 
 #########################################################################################################################
 
@@ -245,18 +262,22 @@ class TorchModels:
 # NOTE: `c` before each method name means that it is custom
 # Feed Forward Neural Network - Keras
 class FFNNKeras:
-    def __init__(self, model_save_path: str = "../chess_models", generate_model_image=False):
+    def __init__(self, model_generator, board_encoder, model_save_path: str = "../chess_models/", generate_model_image=False):
+        self.model: keras.Sequential = model_generator()
+        self.board_encoder = board_encoder
         self.model_save_path = model_save_path
-        self.model_save_path_dir = str(Path(model_save_path).parent)
-        # Create a callback that saves the model's weights
-        self.cp_callback = keras.callbacks.ModelCheckpoint(filepath=self.model_save_path,
+        # self.model_save_path_dir = str(Path(model_save_path).parent)
+
+        # CREATE a callback that saves the model's weights
+        self.cp_callback = keras.callbacks.ModelCheckpoint(filepath=str(Path(self.model_save_path) / "weights.{epoch:06d}-{val_loss:.2f}.hdf5"),
+                                                           save_best_only=True,
                                                            save_weights_only=True,
                                                            verbose=1)
 
-        self.model = self.__generate_model()
+        # PRINT model summary
         # self.model.summary()
 
-        # Save the model graph
+        # SAVE the model graph
         if generate_model_image:
             for i in range(1, 100):
                 if not Path(f'FFNNKeras_{i:03}.png').exists():
@@ -265,40 +286,8 @@ class FFNNKeras:
                     break
         return
 
-    def __generate_model(self):
-        # define the keras model
-        model: keras.Sequential = keras.Sequential()
-        model.add(keras.layers.Dense(256, activation='relu', input_shape=(778,)))
-        model.add(keras.layers.Dense(256, activation='relu'))
-        model.add(keras.layers.Dense(256, activation='relu'))
-        model.add(keras.layers.Dense(256, activation='relu'))
-        model.add(keras.layers.Dense(1, activation='tanh'))
-
-        # compile the keras model
-        # optimizer = keras.optimizers.adam(lr=0.001,
-        #                                        beta_1=0.9,
-        #                                        beta_2=0.999,
-        #                                        epsilon=1e-07,
-        #                                        amsgrad=False, )
-        # __init__(
-        #     learning_rate=0.001,  learning_rate: A Tensor or a floating point value. The learning rate.
-        #     beta_1=0.9,    beta_1: A float value or a constant float tensor. The exponential decay rate for the 1st moment estimates.
-        #     beta_2=0.999,  beta_2: A float value or a constant float tensor. The exponential decay rate for the 2nd moment estimates.
-        #     epsilon=1e-07, epsilon: A small constant for numerical stability. This epsilon is "epsilon hat" in the Kingma
-        #                    and Ba paper (in the formula just before Section 2.1), not the epsilon in Algorithm 1 of the paper.
-        #     amsgrad=False, amsgrad: boolean. Whether to apply AMSGrad variant of this algorithm from the paper "On the Convergence of Adam and beyond".
-        #     name='Adam',   name: Optional name for the operations created when applying gradients. Defaults to "Adam". @compatibility(eager)
-        #                    When eager execution is enabled, learning_rate, beta_1, beta_2, and epsilon can each be a callable that takes no
-        #                    arguments and returns the actual value to use. This can be useful for changing these values across different
-        #                    invocations of optimizer functions. @end_compatibility
-        #     **kwargs       **kwargs: keyword arguments. Allowed to be {clipnorm, clipvalue, lr, decay}. clipnorm is clip gradients by norm;
-        #                    clipvalue is clip gradients by value, decay is included for backward compatibility to allow time inverse decay of
-        #                    learning rate. lr is included for backward compatibility, recommended to use learning_rate instead.
-        # )
-
-        model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])  # mae = Mean Absolute Error
-        return model
-
+    # TODO: to fix this, probably this may not be saving the model correctly/properly due to some parameters or version compatibility problems
+    # REFER: https://github.com/tensorflow/tensorflow/issues/28281
     def c_save_model(self, model_name: str, model_path: Union[str, Path] = "../chess_models"):
         Path(model_path).mkdir(parents=True, exist_ok=True)
         self.model.save(str(Path(model_path) / model_name), overwrite=True)
@@ -310,6 +299,7 @@ class FFNNKeras:
         return
 
     # TODO: to fix this, not working
+    # REFER: https://github.com/tensorflow/tensorflow/issues/28281
     def c_load_model(self, model_name: str, model_path: Union[str, Path] = "../chess_models"):
         if not (Path(model_path) / model_name).exists():
             print(f"ERROR: model does not exists: {Path(model_path) / model_name}")
@@ -323,23 +313,30 @@ class FFNNKeras:
         return
 
     def c_train_model(self, x_input: np.ndarray, y_output: np.ndarray, epochs: int, batch_size: int, validation_split: float):
-        # compile the keras model
         self.model.trainable = True
 
+        # compile the keras model
         # self.model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])  # mae = Mean Absolute Error
-        self.model.fit(x_input, y_output, epochs=epochs, batch_size=batch_size, validation_split=validation_split,
-                       verbose=2, workers=multiprocessing.cpu_count(), use_multiprocessing=True,
-                       callbacks=[self.cp_callback])
+        with tf.device('/gpu:0'):
+            self.model.fit(x_input, y_output, epochs=epochs, batch_size=batch_size, validation_split=validation_split,
+                           verbose=2, workers=multiprocessing.cpu_count(), use_multiprocessing=True,
+                           callbacks=[self.cp_callback])
 
         self.model.trainable = False
         return
 
     def c_evaluate_model(self, x_input_test: np.ndarray, y_output_test: np.ndarray, verbose=2):
-        loss, mae = self.model.evaluate(x_input_test, y_output_test, verbose=2)
+        loss, mae = self.model.evaluate(x_input_test, y_output_test, verbose=verbose)
         print(f"Evaluated: Loss = {loss:5.3f}")
         print(f"Evaluated: MAE = {mae:5.3f}")
 
-    def c_predict(self, encoded_board: np.ndarray) -> np.float32:
+    def c_predict(self, encoded_board: np.ndarray) -> np.ndarray:
+        """
+        Takes a 2D np.ndarray where each row is a chess board made up of the
+        floating point numbers using methods of class `self.board_encoder`
+        :param encoded_board:
+        :return: np.ndarray - 1D
+        """
         return self.model.predict(
             encoded_board,
             verbose=1, workers=multiprocessing.cpu_count(), use_multiprocessing=True
@@ -347,31 +344,116 @@ class FFNNKeras:
 
     def c_predict_board_1(self, board_1: chess.Board) -> np.float32:
         return self.c_predict(
-            step_02.BoardEncoder.encode_board_1_778(
+            self.board_encoder.encode_board_1(
                 board_1
             ).reshape(1, -1)
-        )
+        )[0]
 
-    def c_predict_board_n(self, board_n: Union[List[chess.Board], Tuple[chess.Board]]) -> np.float32:
+    def c_predict_board_n(self, board_n: Union[List[chess.Board], Tuple[chess.Board]]) -> np.ndarray:
         return self.c_predict(
-            step_02.BoardEncoder.encode_board_n_778(
+            self.board_encoder.encode_board_n(
                 board_n
             )
         )
 
     def c_predict_fen_1(self, board_1_fen: str) -> np.float32:
         return self.c_predict(
-            step_02.BoardEncoder.encode_board_1_778(
-                chess.Board(board_1_fen)
+            self.board_encoder.encode_board_1_fen(
+                board_1_fen
             ).reshape(1, -1)
-        )
+        )[0]
 
-    def c_predict_fen_n(self, board_n_fen: Union[List[str], Tuple[str]]) -> np.float32:
+    def c_predict_fen_n(self, board_n_fen: Union[List[str], Tuple[str]]) -> np.ndarray:
         return self.c_predict(
-            step_02.BoardEncoder.encode_board_n_778(
+            self.board_encoder.encode_board_n(
                 [chess.Board(i) for i in board_n_fen]
             )
         )
+
+
+class KerasModels:
+
+    # optimizer = keras.optimizers.adam(lr=0.001,
+    #                                        beta_1=0.9,
+    #                                        beta_2=0.999,
+    #                                        epsilon=1e-07,
+    #                                        amsgrad=False, )
+    # __init__(
+    #     learning_rate=0.001,  learning_rate: A Tensor or a floating point value. The learning rate.
+    #     beta_1=0.9,    beta_1: A float value or a constant float tensor. The exponential decay rate for the 1st moment estimates.
+    #     beta_2=0.999,  beta_2: A float value or a constant float tensor. The exponential decay rate for the 2nd moment estimates.
+    #     epsilon=1e-07, epsilon: A small constant for numerical stability. This epsilon is "epsilon hat" in the Kingma
+    #                    and Ba paper (in the formula just before Section 2.1), not the epsilon in Algorithm 1 of the paper.
+    #     amsgrad=False, amsgrad: boolean. Whether to apply AMSGrad variant of this algorithm from the paper "On the Convergence of Adam and beyond".
+    #     name='Adam',   name: Optional name for the operations created when applying gradients. Defaults to "Adam". @compatibility(eager)
+    #                    When eager execution is enabled, learning_rate, beta_1, beta_2, and epsilon can each be a callable that takes no
+    #                    arguments and returns the actual value to use. This can be useful for changing these values across different
+    #                    invocations of optimizer functions. @end_compatibility
+    #     **kwargs       **kwargs: keyword arguments. Allowed to be {clipnorm, clipvalue, lr, decay}. clipnorm is clip gradients by norm;
+    #                    clipvalue is clip gradients by value, decay is included for backward compatibility to allow time inverse decay of
+    #                    learning rate. lr is included for backward compatibility, recommended to use learning_rate instead.
+    # )
+
+    @staticmethod
+    def model_001() -> keras.Sequential:
+        # define the keras model
+        model: keras.Sequential = keras.Sequential()
+        model.add(keras.layers.Dense(256, activation='relu', input_shape=(778,)))
+        model.add(keras.layers.Dense(256, activation='relu'))
+        model.add(keras.layers.Dense(256, activation='relu'))
+        model.add(keras.layers.Dense(256, activation='relu'))
+        model.add(keras.layers.Dense(1, activation='tanh'))
+
+        # compile the keras model
+        model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])  # mae = Mean Absolute Error
+        return model
+
+    @staticmethod
+    def model_002() -> keras.Sequential:
+        # define the keras model
+        model: keras.Sequential = keras.Sequential()
+        model.add(keras.layers.Dense(512, activation='relu', input_shape=(778,)))
+        model.add(keras.layers.Dense(512, activation='relu'))
+        model.add(keras.layers.Dense(512, activation='relu'))
+        model.add(keras.layers.Dense(512, activation='relu'))
+        model.add(keras.layers.Dense(512, activation='relu'))
+        model.add(keras.layers.Dense(512, activation='relu'))
+        model.add(keras.layers.Dense(1, activation='tanh'))
+
+        # compile the keras model
+        model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])  # mae = Mean Absolute Error
+        return model
+
+    @staticmethod
+    def model_003() -> keras.Sequential:
+        # define the keras model
+        model: keras.Sequential = keras.Sequential()
+        model.add(keras.layers.Dense(1024, activation='relu', input_shape=(778,)))
+        model.add(keras.layers.Dense(1024, activation='relu'))
+        model.add(keras.layers.Dense(1024, activation='relu'))
+        model.add(keras.layers.Dropout(rate=0.1))
+        model.add(keras.layers.Dense(1024, activation='relu'))
+        model.add(keras.layers.Dropout(rate=0.1))
+        model.add(keras.layers.Dense(1024, activation='relu'))
+        model.add(keras.layers.Dropout(rate=0.1))
+        model.add(keras.layers.Dense(1024, activation='relu'))
+        model.add(keras.layers.Dense(1, activation='tanh'))
+
+        # compile the keras model
+        model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])  # mae = Mean Absolute Error
+        return model
+
+    @staticmethod
+    def model_004():
+        inputs = keras.Input(shape=(778,), name='Encoded Chess Board')
+        x = keras.layers.Dense(256, activation='relu')(inputs)
+        x = keras.layers.Dense(256, activation='relu')(x)
+        x = keras.layers.Dense(256, activation='relu')(x)
+        x = keras.layers.Dense(256, activation='relu')(x)
+        outputs = keras.layers.Dense(1, activation='tanh')(x)
+
+        model = keras.Model(inputs=inputs, outputs=outputs, name='chess_778_model_v004')
+        return model
 
 
 #########################################################################################################################
@@ -381,11 +463,11 @@ def get_available_gpus():
 
 
 # GET list of devices
-# get_available_gpus()
-# device_lib.list_local_devices()  # list all local devices
+get_available_gpus()
+# list all local devices
+device_lib.list_local_devices()
 
 # TRAINING on GPU
-# tensorflow.device("/gpu:0")
 # sess = tensorflow.compat.v1.Session(config=tensorflow.compat.v1.ConfigProto(log_device_placement=True))
 if __name__ == '__main__':
     # DATA-SET loading
@@ -394,18 +476,16 @@ if __name__ == '__main__':
         data_x = data[cs.COLUMNS[0]].values
         data_y = data[cs.COLUMNS[1]].values
     with cs.ExecutionTime():
-        data_x_encoded = step_02.BoardEncoder.encode_board_n_778_fen(data_x)
+        data_x_encoded = step_02.BoardEncoder.Encode778.encode_board_n_fen(data_x)
     with cs.ExecutionTime():
-        data_y = data_y * 10000
-        data_y[data_y >= 10] = 10
-        data_y[data_y <= -10] = -10
-        data_y = data_y / 10
+        data_y = step_02.ScoreNormalizer.normalize_001(data_y)
 
     # MODEL creation and training
-    ffnn_keras = FFNNKeras(generate_model_image=True)
-    ffnn_keras.c_load_weights("ffnn_keras_v003_000010_weights.h5")
+    tensorflow.device("/gpu:0")
+    ffnn_keras = FFNNKeras(KerasModels.model_001, board_encoder=step_02.BoardEncoder.Encode778)
+    ffnn_keras.c_load_weights("ffnn_keras_v004_000010_weights.h5")
     with cs.ExecutionTime():
-        ffnn_keras.c_train_model(data_x_encoded, data_y, 10, 512, 0.2)
+        ffnn_keras.c_train_model(data_x_encoded, data_y, 1000, 512, 0.2)
     ffnn_keras.c_save_weights("ffnn_keras_v004_000010_weights.h5")
 
     # MODEL testing
